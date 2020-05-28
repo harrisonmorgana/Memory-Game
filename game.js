cd53b46172b4e8d86b5c652c616be8c77b662d45
harrisonmorgana <harrison.morgana@outlook.com>
2020-05-28 21:04:26 +0000

    Added code

--- game.js
+++ game.js
@@ -1,39 +1,43 @@
-
-$('#myModal').on('shown.bs.modal', function () {
-  $('#myInput').trigger('focus')
-})
-
-const topLeft = document.querySelector('.top-left-box');
-const topMiddle = document.querySelector('.top-middle-box');
-const topRight = document.querySelector('.top-right-box');
-const middleLeft = document.querySelector('.middle-left-box');
-const middleMiddle = document.querySelector('.middle-middle-box');
-const middleRight = document.querySelector('.middle-right-box');
-const bottomLeft = document.querySelector('.bottom-left-box');
-const bottomMiddle = document.querySelector('.bottom-middle-box');
-const bottomRight = document.querySelector('.bottom-right-box');
-
-
-
-const sequence = [topLeft, topMiddle, middleLeft, middleRight bottomMiddle];
-
-const flash = (box) => {
-    return new Promise((resolve, reject) => {
-        box.className += 'active';
-        setTimeout(() => { 
-            box.className = box.className.replace(
-                'active', 
-                ' '
-                );
-                resolve();
-        } , 1000)
-    });
-
+function flash(id) {
+    
 }
 
-const main = async () => {
-    for (let box of sequence)
-    await flash(box)
-};
+//const flash = (box) => {
+//    return new Promise((resolve, reject) => {
+//        box.className += 'active';
+//        setTimeout(() => { 
+//            box.className = box.className.replace(
+//                'active', 
+//                ' '
+//                );
+//                resolve();
+//        } , 1000)
+//    });
+//}
 
-main();
+function sleep(milliseconds) {
+  const date = Date.now();
+  let currentDate = null;
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < milliseconds);
+}
+
+
+function main() {
+    var sequence = ["topLeft", "topCenter", "middleLeft", "middleRight", "bottomCenter"];
+
+    for (box in sequence) {
+        element = document.getElementById(sequence[box]);
+        
+        element.classList.add("active");
+    }
+
+    sleep(1000);
+
+    for (box in sequence) {
+        element = document.getElementById(sequence[box]);
+        
+        element.classList.remove("active");
+    }
+}
